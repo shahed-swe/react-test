ed4bb7826c223e42feec08d7c64db2bb9c05d6d7
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-10-07 22:46:05 +0600

    new commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Here this is the main header portion</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
