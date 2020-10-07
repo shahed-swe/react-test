0930abd682b8d83bb763f584c0c81401b2d7447f
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-10-07 23:01:11 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <h1 className="text-primary">Hello</h1>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div>
+                <h2>This is footer section</h2>
+            </div>
+        );
+    }
+}
